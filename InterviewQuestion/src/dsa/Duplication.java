9b39a7d18bcd18dba2fceae7be0329942b4cadbe
softwarevikasheng <vikashkum84488@gmail.com>
2026-04-13 12:56:33 +0530

    duplication

--- InterviewQuestion/src/dsa/Duplication.java
+++ InterviewQuestion/src/dsa/Duplication.java
@@ -1,10 +1,52 @@
+//package dsa;
+//
+//import java.util.Set;
+//
+//public class Duplication {
+//
+//	public static void main(String[] args) {
+//		String name = "vikashKumar";
+//		String result ="";
+//		for (int i = 0; i < name.length(); i++) {
+//			char data = name.charAt(i);
+//			if(result.indexOf(data)== -1) {
+//				result = result +data;
+//			}
+//			
+//		}
+//		System.out.println(result);
+//	}
+//
+//}
+
+
+
+
 package dsa;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Duplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 String name = "vikashKumar";
+	        String result = "";
 
+	        Set<Character> set = new LinkedHashSet<>();
+
+	        for (char ch : name.toCharArray()) {
+	         
+	                set.add(ch);
+	                
+	            
+	        }
+	        for (Character character : set) {
+			 result+=	character ;
+			}
+
+	        System.out.println(result);
 	}
 
 }
